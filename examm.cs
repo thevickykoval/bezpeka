d62229b1c85fdda1f1cb570aee6405f0dca2708e
thevickykoval <90474512+thevickykoval@users.noreply.github.com>
2021-12-23 11:18:11 +0200

    Add files via upload

---
+++ examm.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Logging;
+static void Main(string[] args)
+    {
+
+        var loggerFactory = LoggerFactory.Create(builder =>
+       {
+           builder
+                .AddFilter("Microsoft", Microsoft.Extensions.Logging.LogLevel.Warning)
+                .AddFilter("System", Microsoft.Extensions.Logging.LogLevel.Warning)
+                .AddConsole()
+                .AddEventLog()
+                .AddDebug();
+       });
+        Microsoft.Extensions.Logging.ILogger logger = loggerFactory.CreateLogger<Program>();
+        logger.LogCritical(1, "The Example LogCritical ({0}).", DateTime.UtcNow);
+        logger.LogError(2, "The Example LogError ({0}).", DateTime.UtcNow);
+        logger.LogWarning(3, "The Example Warning ({0}).", DateTime.UtcNow);
+    }
